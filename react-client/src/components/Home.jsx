dfefc213d207014b92d7529d7f10f271497842cc
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-06-23 14:15:07 -0400

    Add popups to homepage

--- react-client/src/components/Home.jsx
+++ react-client/src/components/Home.jsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 
-export default function Home() {
-  return <div>Welcome home!</div>
+export default class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      popups: []
+    };
+  }
+
+  componentDidMount() {
+    this.getAllPopups();
+  }
+
+  getAllPopups() {
+    axios.get('https://discountedfoodscraps.firebaseio.com/popups.json')
+         .then((response) => {
+            this.setState({ popups: response.data });
+         })
+  }
+
+  renderPopups() {
+    return this.state.popups.map((popup, i) => (
+      <div key={i}>
+        <div>{popup.name}</div>
+        <div>{popup.address}</div>
+      </div>
+    ));
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderPopups()}
+      </div>
+    );
+  }
 }

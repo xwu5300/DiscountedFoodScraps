dd11fe859d5046df62ac42e04d73c91adec6c6f7
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-06-23 15:52:12 -0400

    Remove home image

--- react-client/src/components/Home.jsx
+++ react-client/src/components/Home.jsx
@@ -27,6 +27,7 @@ export default class Home extends React.Component {
       <div key={i}>
         <div><Link to={`/popups/${i}`}>{popup.name}</Link></div>
         <div>{popup.address}</div>
+        <Image src={} ></Image>
       </div>
     ));
   }
@@ -34,10 +35,6 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
-        <Image
-          responsive
-          src="https://firebasestorage.googleapis.com/v0/b/discountedfoodscraps.appspot.com/o/heroes%2FHero%20Home%20page.jpg?alt=media&token=24134335-417e-4c83-9946-48b7fe1341c6"
-        />
         {this.renderPopups()}
       </div>
     );
